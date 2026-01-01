540924de2a791a47de3268f86ad9b89eba771a0a
ichigomomoko <momongawater@tutanota.com>
2026-01-01 13:12:25 +0100

    Add files via upload

---
+++ тест на кроля.py
@@ -0,0 +1,76 @@
+print("тест на сколько процентов ты кроль ૮`⎚⩊⎚´ა")
+print("точность 100% ૮꒰˶മ ˕ മ˶꒱ა")
+procents = 0
+print("отвечать да или нет 🐰")
+print("вопрос: ты кроль? 🐇")
+answer = input("ответ: ")
+if answer == "да":
+    procents = procents + 10
+if answer == "нет":
+    procents = procents + 0
+print("вопрос: ты пушистый? 🍧")
+answer = input("ответ: ")
+if answer == "да":
+    procents = procents + 10
+if answer == "нет":
+    procents = procents + 0
+print("вопрос: у тебя есть длинные ушки? 🐇")
+answer = input("ответ: ")
+if answer == "да":
+    procents = procents + 10
+if answer == "нет":
+    procents = procents + 0
+print("вопрос: ты любишь морковку? 🥕")
+answer = input("ответ: ")
+if answer == "да":
+    procents = procents + 10
+if answer == "нет":
+    procents = procents + 0
+print("вопрос: ты прыгаешь? 🐇")
+answer = input("ответ: ")
+if answer == "да":
+    procents = procents + 10
+if answer == "нет":
+    procents = procents + 0
+print("вопрос: у тебя есть хвост? 🐇")
+answer = input("ответ: ")
+if answer == "да":
+    procents = procents + 10
+if answer == "нет":
+    procents = procents + 0
+print("вопрос: ты ешь сено? 🌱")
+answer = input("ответ: ")
+if answer == "да":
+    procents = procents + 10
+if answer == "нет":
+    procents = procents + 0
+print("вопрос: у тебя есть коготочки? 🐾")
+answer = input("ответ: ")
+if answer == "да":
+    procents = procents + 10
+if answer == "нет":
+    procents = procents + 0
+print("вопрос: ты боишься больших хищников зубастых таких? 🦊")
+answer = input("ответ: ")
+if answer == "да":
+    procents = procents + 10
+if answer == "нет":
+    procents = procents + 0
+print("вопрос: ты живёшь в норе? 🕳️")
+answer = input("ответ: ")
+if answer == "да":
+    procents = procents + 10
+if answer == "нет":
+    procents = procents + 0
+print("ты кроль на " + str(procents) + " процентов! 🐇")
+if procents >= 80:
+    print("ты настоящий кроль!૮₍ ˶ᵔ ᵕ ᵔ˶ ₎ა🥕")
+elif procents >= 50:
+    print("ты наполовину кроль!૮₍ ´• ˕ •` ₎ა")
+elif procents < 50:
+    print("ты не кроль!૮₍•᷄  ༝ •᷅₎ა")
+print("спасибо за прохождение самого точного теста!")
+print("   __")
+print("⊂⊂ • ) ")
+print(" /  | ")
+print("⊂___u")
